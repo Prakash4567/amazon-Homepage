d38427cdeb5579c741d459d192ed93a64dd899fc
Prakash4567 <prakashdsp7777@gmail.com>
2021-01-06 11:20:59 +0530

    amazon-Homepage

---
+++ src/firebas.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC9IhT5HnYVZ-0UaSBknNPNpFc_UdLk7MU",
+    authDomain: "fir-a5982.firebaseapp.com",
+    databaseURL: "https://fir-a5982.firebaseio.com",
+    projectId: "fir-a5982",
+    storageBucket: "fir-a5982.appspot.com",
+    messagingSenderId: "172158235225",
+    appId: "1:172158235225:web:565d6c60eb34f4ac16b748",
+    measurementId: "G-GH8F1430JR"
+  };
